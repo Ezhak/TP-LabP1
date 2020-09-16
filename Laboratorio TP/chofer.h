59721509b67c5282d49851e2aa7ec1af8a7ed943
Code Doctor <71150932+ipetrignani@users.noreply.github.com>
2020-09-16 00:24:58 -0300

    General improvements
    Drop redundant struct FechaVencimiento
    Fix date validation
    Optimize CPU and disk usage for input validation
    Refactor conditionals (use negation), names, spacing

--- Laboratorio TP/chofer.h
+++ Laboratorio TP/chofer.h
@@ -11,11 +11,6 @@ struct Fecha {
 	int mes;
 	int anio;
 };
-struct FechaVencimiento {
-	int dia;
-	int mes;
-	int anio;
-};
 struct Chofer {
 	char dni[10];
 	char apellido[50];
@@ -23,8 +18,8 @@ struct Chofer {
 	char cuit[20];
 	char telefono[15];
 	char vacio;
-	Fecha fechaingreso;
-	FechaVencimiento vencimientoRegistro;
+	Fecha fecha_ingreso;
+	Fecha fecha_vencimiento;
 	int tipoRegistro;
 	bool propietarioAuto;
 	bool estado;
@@ -34,140 +29,144 @@ struct Chofer {
 //Prototipo
 void grabarRegistro(Chofer registro);
 // Alta
-int validarFechas(Chofer registro){
-	time_t tiempo;
-	struct tm* tmPtr;
-	tiempo = time(NULL);
-	tmPtr = localtime(&tiempo); //Con time.h se puede obtener la fecha actual y procedi a guardarlo en variables distintas discriminado por
-	int dia = tmPtr->tm_mday; //Dia,Mes y Año, la misma metodologia que utilice al cargar el registro del struct.
-	int mes = tmPtr->tm_mon + 1;
-	int anio = 1900 + tmPtr->tm_year;
-	//Lo que hago aca es basicamente comparar las dos fechas con un condicional superrebuscado, los return '1111' y '2222' son solo codigos
-	//Puestos por mi, a modo de "bandera" para luego en altaRegistro() validarlos, si los return dan esos numeros, pues, no te deja cargar.
-	if ((registro.fechaingreso.dia > dia) && (registro.fechaingreso.mes > mes) && (registro.fechaingreso.anio > anio)) {
-		return 1111;
+bool validarFechaIngreso(tm fecha_actual, Fecha fecha_ing){
+	struct tm t_ing = {0};
+	t_ing.tm_mday = fecha_ing.dia;
+	t_ing.tm_mon = fecha_ing.mes - 1;
+	t_ing.tm_year = fecha_ing.anio - 1900;
+
+	time_t t1 = mktime(&fecha_actual);
+	time_t t2 = mktime(&t_ing);
+
+	if (difftime(t1,t2) > 0) {
+		std::cout << "AAAAAAAAAAAAAAA" << std::endl;
+		return true;
 	}
-	if ((registro.vencimientoRegistro.dia <= dia) && (registro.vencimientoRegistro.mes <= mes) && (registro.vencimientoRegistro.anio <= anio)) {
-		return 2222;
-	}
-	return 0;
+
+	return false;
 }
-int validarDNI(char* dnibusqueda) {
-	FILE* p;
-	Chofer registro;
-	int posicion = 0;
-	p = fopen("registros.dat", "rb");
-	if (p == NULL) {
-		cout << "Error. :)" << endl;
-		return -1;
-	}
-	//El while itera hasta que no encuentre nada para leer
-	//strcmp compara los dos char, entre registro.dni, y dnibusqueda, si da cero, significa que son IGUALES, por ende
-	//va a retornar '1315' que es un codigo que uso para validar a modo de bandera.
-	while (fread(&registro, sizeof registro, 1, p) == 1) {
-		if ((strcmp(registro.dni, dnibusqueda) == 0) && registro.estado == true) {
-			fclose(p);
-			posicion = 1315;
-			return posicion;
-		}
-		posicion++;
-	}
-	fclose(p);
-	return -1;
+
+bool validarFechaVencimiento(tm fecha_actual, Fecha fecha_venc){
+	struct tm t_venc = {0};
+	t_venc.tm_mday = fecha_venc.dia;
+	t_venc.tm_mon = fecha_venc.mes - 1;
+	t_venc.tm_year = fecha_venc.anio - 1900;
+
+	time_t t1 = mktime(&fecha_actual);
+	time_t t2 = mktime(&t_venc);
+
+	if (difftime(t1,t2) < 0)
+		return true;
+
+	return false;
 }
-int validarCUIT(char* cuitbusqueda) {
-	FILE* p;
+
+bool validarDNI(FILE *fp, char* dnibusqueda) {
 	Chofer registro;
-	int posicion = 0;
-	p = fopen("registros.dat", "rb");
-	if (p == NULL) {
-		cout << "Error. :)" << endl;
-		return -1;
+
+	fseek(fp, 0, SEEK_SET);
+	while (fread(&registro, sizeof registro, 1, fp)) {
+		if (!strcmp(registro.dni, dnibusqueda) && (registro.estado == true))
+			return false;
 	}
-	//El while itera hasta que no encuentre nada para leer
-	//strcmp compara los dos char, entre registro.cuit, y cuitbusqueda, si da cero, significa que son IGUALES, por ende
-	//va a retornar '1315' que es un codigo que uso para validar a modo de bandera.
-	while (fread(&registro, sizeof registro, 1, p) == 1) {
-		if ((strcmp(registro.cuit, cuitbusqueda) == 0) && registro.estado == true) {
-			posicion = 1315;
-			fclose(p);
-			return posicion;
-		}
-		posicion++;
+
+	return true;
+}
+bool validarCUIT(FILE *fp, char* cuitbusqueda) {
+	Chofer registro;
+
+	fseek(fp, 0, SEEK_SET);  // resetear indicador de posición 0 + SEEK_SET (0)
+	while (fread(&registro, sizeof registro, 1, fp)) {
+		if (!strcmp(registro.cuit, cuitbusqueda) && (registro.estado == true))
+			return false;
 	}
-	fclose(p);
-	return -1;
+
+	return true;
 }
 
 Chofer cargarRegistro() {
 	Chofer registro;
-	int pos;
+	char *occurrence;
+
 	cin.ignore();
-	//Lo que se hace con la funcion strspn es comparar los caracteres del parametro 1 y 2, si los dos coinciden, retorna 0
-	//Al retornar 0, lo pongo en un while, mientras que registro.dni[0] por ejemplo, porque POS siempre sera 0 si hay espacio en blanco
-	//Significa que mientras este en 0, va a pedir que se ingrese algo. 
+
+	// strspn devuelve el largo de la porción inicial de str1 que sólo
+	// contenga caracteres de str2 (overkill).
+	//
+	// strpbrk devuelve un puntero a la primera ocurrencia en str1 de
+	// cualquier caracter en str2. Evita espacios o <Tab> más simple,
+	// y si es una cadena vacía, registro.campo[0] valdrá '\0'.
+
 	do {
 		cout << "DNI: ";
 		cin.getline(registro.dni, 10);
-		pos = strspn(registro.dni, " \t");
-	} while (registro.dni[pos] == 0);
+		occurrence = strpbrk(registro.dni, " \t");
+	} while (occurrence || !registro.dni[0]);
+
 	do {
 		cout << "Apellido: ";
 		cin.getline(registro.apellido, 50);
-		pos = strspn(registro.apellido, " \t");
-	} while (registro.apellido[pos] == 0);
+		occurrence = strpbrk(registro.apellido, " \t");
+	} while (occurrence || !registro.apellido[0]);
+
 	do {
 		cout << "Nombre: ";
 		cin.getline(registro.nombre, 50);
-		pos = strspn(registro.nombre, " \t");
-	} while (registro.nombre[pos] == 0);
+		occurrence = strpbrk(registro.nombre, " \t");
+	} while (occurrence || !registro.nombre[0]);
+
 	do {
 		cout << "CUIT: ";
 		cin.getline(registro.cuit, 30);
-		pos = strspn(registro.cuit, " \t");
-	} while (registro.cuit[pos] == 0);
+		occurrence = strpbrk(registro.cuit, " \t");
+	} while (occurrence || !registro.cuit[0]);
+
 	cout << "Telefono: " << endl;
 	cin.getline(registro.telefono, 15);
 	cout << "Fecha de Ingreso: " << endl;
 	cout << "Dia: " << endl;
-	cin >> registro.fechaingreso.dia;
+	cin >> registro.fecha_ingreso.dia;
 	cout << "Mes: " << endl;
-	cin >> registro.fechaingreso.mes;
+	cin >> registro.fecha_ingreso.mes;
 	cout << "Anio: " << endl;
-	cin >> registro.fechaingreso.anio;
+	cin >> registro.fecha_ingreso.anio;
 	cout << "Fecha de vencimiento del registro: " << endl;
 	cout << "Dia: " << endl;
-	cin >> registro.vencimientoRegistro.dia;
+	cin >> registro.fecha_vencimiento.dia;
 	cout << "Mes: " << endl;
-	cin >> registro.vencimientoRegistro.mes;
+	cin >> registro.fecha_vencimiento.mes;
 	cout << "Anio: " << endl;
-	cin >> registro.vencimientoRegistro.anio;
+	cin >> registro.fecha_vencimiento.anio;
 	cout << "Tipo de registro: (1-3)" << endl;
 	cin >> registro.tipoRegistro;
 	registro.estado = true;
+
 	return registro;
 }
 void altaRegistro() {
-	bool validarGrabado = true; bool espaciosVacios = false; int posicionDNI; int posicionCUIT; int posicionFechaIngreso; int posicionFechaVencimiento;
+	time_t timer = time(0);
+	// localtime() devuelve un puntero a la estructura estática interna
+	// std::tm.
+	// Recordar que el operador 'struct->object' es idéntico a '(*struct).object'
+	struct tm* tmPtr = localtime(&timer);
+
+	bool validarGrabado = true;
 	Chofer reg;
-	//Cargo el registro en la memoria
+
 	reg = cargarRegistro();
-	//Creo archivo
-	FILE* p;
-	p = fopen("registros.dat", "ab");
-	if (p == NULL) {
+
+	FILE* fp;
+	fp = fopen("registros.dat", "a+b");
+	if (!fp) {
 		cout << "Error. :)" << endl;
 		validarGrabado = false;
 		return;
 	}
-	//-------Grabo el registro de memoria al archivo
-	//Primero tenemos que validar si es la segunda vuelta, la primera no se valida, solo los espacios vacios.
-	//Valido que no haya un DNI o CUIT igual o que no haya un espacio vacio
-	posicionDNI = validarDNI(reg.dni); posicionCUIT = validarCUIT(reg.cuit);
-	posicionFechaIngreso = validarFechas(reg); posicionFechaVencimiento = validarFechas(reg);
-	//Las variables de tipo posicionALGO, sirven para usarlos a modo de bandera, que segun cada funcion, yo le di un codigo particular
-	//Asi, verifico si se cumple tal o cual condicion para validar, DNI no repetido, CUIT no repetido, y las Fechas, etc.
-	if (posicionCUIT == 1315 || posicionDNI == 1315 || posicionFechaIngreso == 1111 || posicionFechaVencimiento == 2222) {
+
+	if (!validarDNI(fp, reg.dni) ||
+	    !validarCUIT(fp, reg.cuit) ||
+	    !validarFechaIngreso(*tmPtr, reg.fecha_ingreso) ||
+	    !validarFechaVencimiento(*tmPtr, reg.fecha_vencimiento)) {
 		cout << "Ya existe ese DNI o CUIT, por favor, ingrese otro DNI o CUIT" << endl;
 		cout << "O bien la Fecha de Ingreso/Vencimiento es invalida." << endl;
 		anykey();
@@ -175,24 +174,24 @@ void altaRegistro() {
 	}
 	else {
 		grabarRegistro(reg);
-		if (validarGrabado == true) {
+		if (validarGrabado)
 			cout << "Registros grabados en archivo correctamente!" << endl << endl;
-		}
-		else {
+		else
 			cout << "No se pudo grabar. :)" << endl << endl;
-		}
-	}	
-	fclose(p);
+	}
+	fclose(fp);
 }
 void grabarRegistro(Chofer registro) {
-	FILE* p;
-	p = fopen("registros.dat", "ab");
-	if (p == NULL) {
+	FILE* fp;
+
+	fp = fopen("registros.dat", "ab");
+	if (!fp) {
 		cout << "Error. :)" << endl;
 		return;
 	}
-	fwrite(&registro, sizeof registro, 1, p);
-	fclose(p);
+
+	fwrite(&registro, sizeof registro, 1, fp);
+	fclose(fp);
 	return;
 }
 // Baja
@@ -250,11 +249,11 @@ int modificarRegistro() {
 		if ((strcmp(registro.dni, dnibusqueda) == 0) && registro.estado == true) { //Explicado en bajaRegistro()
 			cout << "Ingrese la nueva fecha de vencimiento" << endl; //Una vez que encuentra la coincidencia entre registro.dni, con busquedadni,
 			cout << "Dia: " << endl;								// Te deja cargar la nueva fecha, y asi lo modificas.
-			cin >> registro.vencimientoRegistro.dia;
+			cin >> registro.fecha_vencimiento.dia;
 			cout << "Mes: " << endl;
-			cin >> registro.vencimientoRegistro.mes;
+			cin >> registro.fecha_vencimiento.mes;
 			cout << "Anio: " << endl;
-			cin >> registro.vencimientoRegistro.anio;
+			cin >> registro.fecha_vencimiento.anio;
 			fseek(p, pos * sizeof registro, 0); //Explicado ya en bajaRegistro()
 			fwrite(&registro, sizeof registro, 1, p);
 			fclose(p);
@@ -275,13 +274,13 @@ void mostrarRegistro(Chofer registro) {
 		cout << "CUIT: " << registro.cuit << endl;
 		cout << "Telefono: " << registro.telefono << endl;
 		cout << "Fecha de Ingreso: " << endl;
-		cout << "\tDia: " << registro.fechaingreso.dia << endl;
-		cout << "\tMes: " << registro.fechaingreso.mes << endl;
-		cout << "\tAnio: " << registro.fechaingreso.anio << endl;
+		cout << "\tDia: " << registro.fecha_ingreso.dia << endl;
+		cout << "\tMes: " << registro.fecha_ingreso.mes << endl;
+		cout << "\tAnio: " << registro.fecha_ingreso.anio << endl;
 		cout << "Fecha de vencimiento del registro: " << endl;
-		cout << "\tDia: " << registro.vencimientoRegistro.dia << endl;
-		cout << "\tMes: " << registro.vencimientoRegistro.mes << endl;
-		cout << "\tAnio: " << registro.vencimientoRegistro.anio << endl;
+		cout << "\tDia: " << registro.fecha_vencimiento.dia << endl;
+		cout << "\tMes: " << registro.fecha_vencimiento.mes << endl;
+		cout << "\tAnio: " << registro.fecha_vencimiento.anio << endl;
 		cout << "Tipo de registro (1-3): " << registro.tipoRegistro << endl;
 		cout << "----------------------------------------" << endl;
 	}
